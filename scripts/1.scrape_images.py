0f9d5fb33dcba30bd61e53d39b192489fa5476b9
Sana-Harshitha <sanaharshitha2@gmail.com>
2025-06-03 18:07:09 +0530

    succesfully scraped the images

---
+++ scripts/1.scrape_images.py
@@ -0,0 +1,14 @@
+from icrawler.builtin import GoogleImageCrawler
+from celeb_list import celebs
+import os
+
+def download_images(celebs, max_num=40):
+    for celeb in celebs:
+        folder = f'../data/images/{celeb.replace(" ", "_")}'
+        os.makedirs(folder, exist_ok=True)
+        print(f"Downloading for: {celeb}")
+        crawler = GoogleImageCrawler(storage={'root_dir': folder})
+        crawler.crawl(keyword=celeb, max_num=max_num, min_size=(200, 200))
+
+if __name__ == '__main__':
+    download_images(celebs, max_num=30)  # Change to 10–30 if it's slow
